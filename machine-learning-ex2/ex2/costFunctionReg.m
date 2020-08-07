478928ec931400651d0f15cc3d983b831c637f22
Tan Dao <tandao311@gmail.com>
2020-08-07 21:15:59 +0700

    solved predict.m

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,7 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
+J = costFunction(theta, X, y) + lamdba / (2*m) * theta' * theta;
 
 
 
