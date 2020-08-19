e1842b541b4b91915c52248413463c2854dd6a9d
Tan Dao <tandao311@gmail.com>
2020-08-19 17:54:57 +0700

    regularized backprop gradient

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -93,6 +93,8 @@ for t = 1:m
 	Delta1 = Delta1 + error_2(2 : end) * X(t, :);
 	Theta2_grad = 1/m * Delta2;
 	Theta1_grad = 1/m * Delta1;
+	Theta2_grad(:, 2 : end) = Theta2_grad(:, 2 : end) + lambda/m * Theta2(:, 2 : end);
+        Theta1_grad(:, 2 : end) = Theta1_grad(:, 2 : end) + lambda/m * Theta1(:, 2 : end);
 end
 
 % =========================================================================
