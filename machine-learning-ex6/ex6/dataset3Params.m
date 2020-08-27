f6b19181c97aefb7531ed4ad8571315b84b9cb39
Tan Dao <tandao311@gmail.com>
2020-08-27 16:38:07 +0700

    selecting best C and sigma for validation set

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,9 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+steps = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+C = 0.01;
+sigma = 0.01;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,10 +23,26 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+min_error = 100;
+fprintf('Selecting C and sigma:\nC\tsigma\tError validation\n');
+for i = 1 : size(steps)
+	C_temp = steps(i);
+	for j = 1 : size(steps)
+		sigma_temp = steps(j);
+		model = svmTrain(X, y, C_temp, @(x1,x2) gaussianKernel(x1, x2, sigma_temp));
+		predictions = svmPredict(model, Xval);
+		error_validation = mean(double(predictions ~= yval));
+	
+		if(error_validation < min_error)
+			C = C_temp;
+			sigma = sigma_temp;
+			min_error = error_validation;
+		endif
 
+	end
+end
 
-
-
+fprintf('%f\t%f\t%f\n', C, sigma, error_validation);
 
 
 
